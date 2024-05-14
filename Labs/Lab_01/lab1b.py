7156c1823894be5b3329dcd28d5ebf9ae0596381
Yasas Maddumage <yasasrm@gmail.com>
2024-05-14 10:34:52 -0400

    Comment added

--- Labs/Lab_01/lab1b.py
+++ Labs/Lab_01/lab1b.py
@@ -5,10 +5,12 @@ Description: Lab 1 Question 2 (lab1a.py).
 ''' 
 
 
-print("Welcome to PRG600!") 
-user_name = input("Enter your name:") 
-student_id = input("Enter your student ID:") 
-age = input("Enter your age:") 
-favorite_food = input("Enter your favorite food:") 
+print("Welcome to PRG600!")                     #Greeting
+user_name = input("Enter your name:")           #Get user name as user input and stored to user_name variable
+student_id = input("Enter your student ID:")    #Get student id as user input and stored to student_id variable 
+age = input("Enter your age:")                  #Get age as user input and stored to age variable
+favorite_food = input("Enter your favorite food:") #Get favorite food as user input and stored to favorite_food variable
+
+#Final output messages
 print(user_name + " is " + age + " years old and loves to eat "+favorite_food+"!") 
 print("Good luck with the course!")

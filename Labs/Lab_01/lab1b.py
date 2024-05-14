752e9d7450e451016fd7692471a5ec8b886fd7e0
Yasas Maddumage <yasasrm@gmail.com>
2024-05-14 10:07:14 -0400

    lab1b initial code

---
+++ Labs/Lab_01/lab1b.py
@@ -0,0 +1,14 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 1 Question 2 (lab1a.py).  
+''' 
+
+
+print("Welcome to PRG600!") 
+user_name = input("Enter your name:") 
+student_id = input("Enter your student ID:") 
+age = ("Enter your age:") 
+favorite_food = ("Enter your favorite food:") 
+print(user_name + " is " + age + " years old and loves to eat "+favorite_food+"!") 
+print("Good luck with the course!")

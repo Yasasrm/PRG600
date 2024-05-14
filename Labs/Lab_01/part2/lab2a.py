0623ce2e52bd6ef72df3f6f3ba49ec717d3ca0b6
Yasas Maddumage <yasasrm@gmail.com>
2024-05-14 11:01:07 -0400

    Lab2a created

---
+++ Labs/Lab_01/part2/lab2a.py
@@ -0,0 +1,14 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 1 part 2 Question 2 (lab2a.py).  
+''' 
+
+
+
+user_num1 = input("Enter the first number:")
+user_num2 = input("Enter the second number:") 
+user_int1 = int(user_num1)
+user_int2 = int(user_num2)
+result = user_int1 + user_int2
+print("The result of "+user_num1+" plus "+user_num2+" is: "+str(result))

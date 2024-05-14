f61d9b9e08ec7a2b7ff644c0e5fb50c1d444bc0e
Yasas Maddumage <yasasrm@gmail.com>
2024-05-14 11:04:17 -0400

    lab2a comment added

--- Labs/Lab_01/part2/lab2a.py
+++ Labs/Lab_01/part2/lab2a.py
@@ -6,9 +6,9 @@ Description: Lab 1 part 2 Question 2 (lab2a.py).
 
 
 
-user_num1 = input("Enter the first number:")
-user_num2 = input("Enter the second number:") 
-user_int1 = int(user_num1)
-user_int2 = int(user_num2)
-result = user_int1 + user_int2
-print("The result of "+user_num1+" plus "+user_num2+" is: "+str(result))
+user_num1 = input("Enter the first number:")        #get first number as user input
+user_num2 = input("Enter the second number:")       #get second number as user input 
+user_int1 = int(user_num1)                          #cast first user input number to integer
+user_int2 = int(user_num2)                          #cast second user input number to integer
+result = user_int1 + user_int2                      #calculate sum
+print("The result of "+user_num1+" plus "+user_num2+" is: "+str(result))    #final output

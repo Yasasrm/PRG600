09adbec17085dc9180f59517f0e5bc8d1823490c
Yasas Maddumage <yasasrm@gmail.com>
2024-05-14 12:20:09 -0400

    lab2b comment added

--- Labs/Lab_01/part2/lab2b.py
+++ Labs/Lab_01/part2/lab2b.py
@@ -4,13 +4,15 @@ Student ID: 170308233.
 Description: Lab 1 part 2 Question 3 (lab2b.py).  
 ''' 
 
-gross_income = input("Enter your gross income:") 
-dependents = input("Enter the number of dependents:") 
-standerd_deduction = 10000
-dependent_deduction = 2000 * int(dependents)
-taxable_income = int(gross_income) - standerd_deduction - dependent_deduction
-income_tax = 0
 
+gross_income = input("Enter your gross income:")        #get gross income as user input
+dependents = input("Enter the number of dependents:")   #get dependents as user input 
+standerd_deduction = 10000                              #defined standerd deduction
+dependent_deduction = 2000 * int(dependents)            #calculate deduction due to number of dependents
+taxable_income = int(gross_income) - standerd_deduction - dependent_deduction #calculate taxable income
+income_tax = 0  #defined income tax
+
+#Calculate incometax - start
 if taxable_income < 32000:
     income_tax = taxable_income * 0.1
 elif 32000 <= taxable_income <= 64000:
@@ -20,5 +22,6 @@ else:
 
 if income_tax < 0:
     income_tax = 0
+#Calculate incometax - end
 
-print("The income tax is $"+str(round(income_tax))+".") 
+print("The income tax is $"+str(round(income_tax))+".")     #final output

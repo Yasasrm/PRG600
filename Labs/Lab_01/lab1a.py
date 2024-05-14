4a28f27eb2742759944ed671ee0b8a296401b9c2
Yasas Maddumage <yasasrm@gmail.com>
2024-05-13 23:46:03 -0400

    Lab 1 a

---
+++ Labs/Lab_01/lab1a.py
@@ -0,0 +1,9 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 1 Question 1 (lab1a.py).  
+''' 
+ 
+print("Hello World!") 
+user_name = input("Please enter your name: ")  # asks user for input 
+print("Hello " + user_name + "!")  # prints user name in a friendly way.

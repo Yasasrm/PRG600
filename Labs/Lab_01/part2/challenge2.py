c789367b6c880d15b94cacb810e6aaea0c404ae3
Yasas Maddumage <yasasrm@gmail.com>
2024-05-14 12:50:35 -0400

    challenge2 comment added

--- Labs/Lab_01/part2/challenge2.py
+++ Labs/Lab_01/part2/challenge2.py
@@ -4,11 +4,12 @@ Student ID: 170308233.
 Description: Lab 1 challenge (challenge2.py).  
 ''' 
 
-numberToBeRoundUp = input("Enter a number:")
-floatNumberToBeRoundUp = float(numberToBeRoundUp)
-intNumberToBeRoundUp = int(floatNumberToBeRoundUp)
-decimalPoints = floatNumberToBeRoundUp-intNumberToBeRoundUp
+numberToBeRoundUp = input("Enter a number:")                #Get a number as user input
+floatNumberToBeRoundUp = float(numberToBeRoundUp)           #Convert user input to a float
+intNumberToBeRoundUp = int(floatNumberToBeRoundUp)          #Get the integer part of the user input value
+decimalPoints = floatNumberToBeRoundUp-intNumberToBeRoundUp #Get the decimal points of the user input value 
 
+#Check the decimal point > 0.5 and add 1 or remove decimal parts from the output and print the output
 if decimalPoints < 0.5:
     print("Rounded value is: "+str(intNumberToBeRoundUp))
 else:

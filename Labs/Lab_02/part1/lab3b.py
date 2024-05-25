7ccb9c5c7269970f77f897f849f62e3051e70cc7
Yasas Maddumage <yasasrm@gmail.com>
2024-05-25 17:35:40 -0400

    Lab 2 Q2 created

---
+++ Labs/Lab_02/part1/lab3b.py
@@ -0,0 +1,11 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 2 part 2 Question 1 (lab3b.py).  
+''' 
+count = 0
+while count < 11:
+    print(count)
+    count+=1 #Count increment
+
+print("finished")

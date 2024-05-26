b0d0d3c7e3f1b796082e58c6c570ddb47d58b128
Yasas Maddumage <yasasrm@gmail.com>
2024-05-25 22:58:26 -0400

    Comment added

--- Labs/Lab_02/part1/lab3e.py
+++ Labs/Lab_02/part1/lab3e.py
@@ -7,28 +7,32 @@ Math Quiz v2.0
 
 from random import randint
 
+#global variable initialization
 firstNumber = 0
 secondNumber = 0
 points = 0
 questionCount = 0
 
+#start
 def main():
     quizeCycle(getNewQuizQuestion())
-    
+
+#quiz question cycle until press q    
 def quizeCycle(ques):
     answer = getAnswerFromUser(ques)
-    if answer == "q":
+    if answer == "q": #exit from the question loop
         printScore()
-    elif answer == "s":
+    elif answer == "s": #skip to next question
         getNextQuestion()
-    elif int(answer) == (firstNumber + secondNumber):
+    elif int(answer) == (firstNumber + secondNumber): #calculate points and go to next question
         calculatePoints()
         getNextQuestion()
         print("Next question...")
     else:
-        print("Incorrect. Try again.")
+        print("Incorrect. Try again.") #repeat same question
         quizeCycle(ques)
 
+#return new question
 def getNewQuizQuestion():
   global firstNumber
   firstNumber = randint(1,10)
@@ -36,23 +40,26 @@ def getNewQuizQuestion():
   secondNumber = randint(1,10)
   return "Enter the answer to %s + %s, press 's' to skip or 'q' to quit: " % (firstNumber, secondNumber)  #Question
 
-        
+#return user's input (Assumption: User will only insert numbers, q or s)       
 def getAnswerFromUser(ques):
     return input(ques)
 
+#print final score
 def printScore():
     print("Quiz over. You scored {percent:.1f}%.".format(percent = (points/questionCount)*100))
 
+#move to next question
 def getNextQuestion():
     global questionCount
     questionCount += 1
     quizeCycle(getNewQuizQuestion())
 
+#points calculator
 def calculatePoints():
     print("Correct! You have been awarded 1 point!") 
     global points
     points += 1
 
-
+#entry point
 if __name__ == "__main__":
     main()

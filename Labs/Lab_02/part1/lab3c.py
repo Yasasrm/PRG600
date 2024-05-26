8cf7ba8e67d7a8cdbc57a1c4937dce447c5fa3c2
Yasas Maddumage <yasasrm@gmail.com>
2024-05-25 20:19:46 -0400

    Lab 2 P1 Q4 created

--- Labs/Lab_02/part1/lab3c.py
+++ Labs/Lab_02/part1/lab3c.py
@@ -1,17 +1,17 @@
 ''' 
 Name: Yasas Maddumage. 
 Student ID: 170308233. 
-Description: Lab 2 part 1 Question 3 (lab3b.py).  
+Description: Lab 2 part 1 Question 3 (lab3c.py).  
 ''' 
 
 sum = 0 
 print("SUMMING CALCULATOR") 
 while True: 
     print("The sum so far: " + str(sum)) 
-    user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ") 
-    if user_input == "": 
+    user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ") #get user input
+    if user_input == "": #exit point
         break 
-    else:  
+    else:  #summing calculation
         sum += int(user_input) 
-print("Thank you for using summing calculator. The final sum was " + str(sum) 
+print("Thank you for using summing calculator. The final sum was " + str(sum) #output
 + ".")
---
+++ Labs/Lab_02/part1/lab3d.py
@@ -0,0 +1,20 @@
+import random
+from random import randint
+
+
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 2 part 1 Question 4 (lab3d.py).  
+''' 
+
+#variable initialization
+secret = randint(1,10)
+
+while True:
+    user_guess = input("Guess a number between 1 and 10:") #user input
+    if secret == int(user_guess):
+        print("Correct! You win.")
+        break #exit point
+    else:
+        print("Sorry, that's not it.") #loop continues

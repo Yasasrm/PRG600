873a7412f368aca85c239ebcc600a9f6464f46d7
Yasas Maddumage <yasasrm@gmail.com>
2024-05-26 15:25:50 -0400

    binary converter function

--- Labs/Lab_02/part1/challenge3.py
+++ Labs/Lab_02/part1/challenge3.py
@@ -4,9 +4,13 @@ Student ID: 170308233.
 Description: Lab 2 part 1 Question 6 (challenge3.py).
 ''' 
 
+binNumber = "0b"
+
 def main():
     decimalNumber = input("Enter a decimal number: ")
     binaryConverter(decimalNumber)
+    print(binNumber)
+    print(bin(int(decimalNumber)))
 
 
 def binaryConverter(dcml):
@@ -14,7 +18,12 @@ def binaryConverter(dcml):
     remainder = int(dcml)%2
     if quotient != 1:
         binaryDigir = remainder
-        binaryConverter(binaryDigir)
+        binaryConverter(quotient)
     else:
-        binaryDigir = quotient
-    return 0
+        binaryDigir = round(quotient)
+    global binNumber
+    binNumber += str(binaryDigir)
+
+#entry point
+if __name__ == "__main__":
+    main()

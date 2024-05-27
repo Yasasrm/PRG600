82ff39b7606680e251d815290c6dc469e78f8602
Yasas Maddumage <yasasrm@gmail.com>
2024-05-26 21:57:57 -0400

    bug fix(circle_area)

--- Labs/Lab_02/part2/lab4c.py
+++ Labs/Lab_02/part2/lab4c.py
@@ -6,7 +6,7 @@ Description: Lab 2 part 2 Question 3 (lab4c.py).
 
 import math
 
-def getArea(radius):
+def circle_area(radius):
     return math.pi * radius**2
 
 if __name__ == "__main__":
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     while True:
         radius = input("Enter a radius between 0 and 1999. Press Enter to exit:")
         if radius.isnumeric() and 0 <= int(radius) < 1000:
-            area = getArea(int(radius))
+            area = circle_area(int(radius))
             print("Radius: %s. Area: %s." %(radius,area))
         elif radius == "" and cycle > 0:
             print("Exiting...")

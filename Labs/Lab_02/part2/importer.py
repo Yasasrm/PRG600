17446c71c497b458c334606ccb48c7e8974fa774
Yasas Maddumage <yasasrm@gmail.com>
2024-05-26 20:28:54 -0400

    function call from another file

---
+++ Labs/Lab_02/part2/importer.py
@@ -0,0 +1,9 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 2 part 2 Question 2.2 (importer.py).  
+''' 
+
+from lab4b import is_odd
+
+print(is_odd(27))
--- Labs/Lab_02/part2/lab4b.py
+++ Labs/Lab_02/part2/lab4b.py
@@ -10,24 +10,15 @@ def rtrn_area(length, width): # Parameters- Length, Width | Returns- Area
     '''When given length and width, area will be calculated''' 
     return length * width 
 
-rect = rtrn_area(4, 3) # call the function again with new values
-print(rect) 
-
 def print_all_caps(name, age): # Parameters- Name, Age | Returns- Nothing  
     '''When given name and age, caption will be printed, name will be converted to all capital'''
     cap_name = name.upper() 
     print('THIS PERSON\'S NAME IS ' + cap_name + ' AND THEY ARE ' + str(age) + ' YEARS OLD!!!')
     return
-
-print_all_caps('yasas', 30) 
-print_all_caps('ron', 40) # call the function again with new values 
     
 def get_rando(): # Parameters- Nothing | Returns- Nothing  
     '''generate a random number between 1 - 101'''
     return randint(1, 101) 
-
-lucky_num = get_rando() # call the function again with new values 
-print(lucky_num)
 
 def is_odd(num): # Parameters- Number(num) | Returns- Boolean  
     '''Check whether the number odd or even''' 
@@ -36,5 +27,13 @@ def is_odd(num): # Parameters- Number(num) | Returns- Boolean
     else: 
         return False 
     
-print(is_odd(13)) 
-print(is_odd(get_rando())) # call the function again with new values
+if __name__ == "__main__": 
+    print(is_odd(13)) # put the rest of your function calls here.
+    rect = rtrn_area(4, 3) # call the function again with new values
+    print(rect) 
+    print_all_caps('yasas', 30) 
+    print_all_caps('ron', 40) # call the function again with new values 
+    lucky_num = get_rando() # call the function again with new values 
+    print(lucky_num)
+    print(is_odd(13)) 
+    print(is_odd(get_rando())) # call the function again with new values

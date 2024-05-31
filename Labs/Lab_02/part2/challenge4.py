1ada59ba73ad8c197196791b50f10535bb1db597
Yasas Maddumage <yasasrm@gmail.com>
2024-05-31 18:59:04 -0400

    Comment added

--- Labs/Lab_02/part2/challenge4.py
+++ Labs/Lab_02/part2/challenge4.py
@@ -4,42 +4,42 @@ Student ID: 170308233.
 Description: Lab 2 part 2 Question 4 (challenge.py).  
 ''' 
 
-def rtrn_slope(x1, y1, x2, y2):
+def rtrn_slope(x1, y1, x2, y2): #calculate slope
     if (x2 - x1) != 0:
         return (y2 - y1)/(x2 - x1)
     else:
         return "tan(90)"
 
-def isValidCoordinate(input):
-    if input.isnumeric() and 0 <= int(input) < 11:
+def isValidCoordinate(input): #validate coordinates
+    if input.isnumeric() and 0 <= int(input) < 11: #check numeric range
         return True
-    elif not input.isnumeric():
+    elif not input.isnumeric(): #if not numeric returns error
         print("Error: %s is not a number." %input)
         return False
     else:
-        print("Error: %s is out of bounds." %input)
+        print("Error: %s is out of bounds." %input) #if out of range returns error
         return False
 
 if __name__ == "__main__":
     while True:
-        x1 = input("Enter the starting X value:")
+        x1 = input("Enter the starting X value:") #get X1
         if isValidCoordinate(x1):
             break
 
     while True:
-        y1 = input("Enter the next Y value:")
+        y1 = input("Enter the next Y value:") #get Y1
         if isValidCoordinate(y1):
             break
     
     while True:
         while True:
-            x2 = input("Enter the starting X value:")
+            x2 = input("Enter the starting X value:") #get X2
             if isValidCoordinate(x2):
                 break
         
         while True:
-            y2 = input("Enter the next Y value:")
+            y2 = input("Enter the next Y value:") #getY2
             if isValidCoordinate(y2):
                 break
 
-        print("Slope is: "+str(rtrn_slope(int(x1), int(y1), int(x2), int(y2))))
+        print("Slope is: "+str(rtrn_slope(int(x1), int(y1), int(x2), int(y2)))) #final output
--- Labs/Lab_02/part2/lab4c.py
+++ Labs/Lab_02/part2/lab4c.py
@@ -6,7 +6,7 @@ Description: Lab 2 part 2 Question 3 (lab4c.py).
 
 import math
 
-def circle_area(radius):
+def circle_area(radius): #calculate circle area
     return math.pi * radius**2
 
 if __name__ == "__main__":
@@ -14,19 +14,19 @@ if __name__ == "__main__":
     cycle = 0
     while True:
         radius = input("Enter a radius between 0 and 1999. Press Enter to exit:")
-        if radius.isnumeric() and 0 <= int(radius) < 1000:
+        if radius.isnumeric() and 0 <= int(radius) < 1000: #validate radius range
             area = circle_area(int(radius))
             print("Radius: %s. Area: %s." %(radius,area))
-        elif radius == "" and cycle > 0:
+        elif radius == "" and cycle > 0: #exit point
             print("Exiting...")
             break
-        elif radius == "" and cycle == 0:
+        elif radius == "" and cycle == 0: #exit point (first time)
            print("Program was cancelled.")
            break 
         elif radius.isnumeric():
-            print("Error: %s is out of bounds." %radius)
+            print("Error: %s is out of bounds." %radius) #if radius not in the range, returns an error
         else:
-            print("Error: %s is not a number." %radius)
+            print("Error: %s is not a number." %radius) #if radius not a numeric value, returns an error
         cycle+=1
 
 

afcbccf14f2d8ca4b119728c828949521ac962e3
Yasas Maddumage <yasasrm@gmail.com>
2024-05-31 19:03:50 -0400

    Comment added

--- Labs/Lab_02/part1/challenge3.py
+++ Labs/Lab_02/part1/challenge3.py
@@ -6,7 +6,7 @@ Description: Lab 2 part 1 Question 6 (challenge3.py).
 
 binNumber = "0b"
 
-def binaryConverter(dcml):
+def binaryConverter(dcml): #convert decimal to binary
     quotient = int(dcml)/2
     remainder = int(dcml)%2
     if quotient > 0:
@@ -17,7 +17,7 @@ def binaryConverter(dcml):
 
 #entry point
 if __name__ == "__main__":
-    decimalNumber = input("Enter a decimal number: ")
-    binaryConverter(decimalNumber)
-    print(binNumber)
-    print(bin(int(decimalNumber)))
+    decimalNumber = input("Enter a decimal number: ") #get decimal number
+    binaryConverter(decimalNumber) #convert decimal number to binary with the custom fuction
+    print(binNumber) #print custom function binary value
+    print(bin(int(decimalNumber))) #print buildin function binary value

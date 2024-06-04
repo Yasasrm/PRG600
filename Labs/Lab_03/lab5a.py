48507949c3dbd58f6036d443b66aa8f3e0f4daa5
Yasas Maddumage <yasasrm@gmail.com>
2024-06-03 22:54:11 -0400

    lab3 Q1 first step

---
+++ Labs/Lab_03/lab5a.py
@@ -0,0 +1,16 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 3 Question 1 (lab5a.py).  
+''' 
+
+user_numbers = [] 
+print("SUMMING CALCULATOR") 
+while True: 
+    user_input = input("Enter a number to add to your sum. Pressing Enter will exit. ") 
+    if user_input == "": 
+        break 
+    else:  
+        user_numbers.append(int(user_input)) 
+print(user_numbers) 
+print("Thank you for using summing calculator. The final sum was " + str(sum(user_numbers)) + ".") 

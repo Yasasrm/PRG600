1725757f87d3e65562a2d570a19bff5aa07dc5aa
Yasas Maddumage <yasasrm@gmail.com>
2024-06-03 23:30:33 -0400

    Lab3 Q2

---
+++ Labs/Lab_03/lab5b.py
@@ -0,0 +1,10 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 3 Question 2 (lab5b.py).  
+''' 
+
+animals = ['snake', 'hamster', 'scorpion', 'beaver', 'mosquito', 'camel', 'vulture', 'horse', 'python', 'capybara' ]
+
+for i in range(1,len(animals),2):
+    print(animals[i])

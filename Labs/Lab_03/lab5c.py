ed8379e4b34dd4802e35560cc04efe88a872ce6b
Yasas Maddumage <yasasrm@gmail.com>
2024-06-04 14:06:12 -0400

    Lab3 Q3 bugfix and comment added

--- Labs/Lab_03/lab5c.py
+++ Labs/Lab_03/lab5c.py
@@ -7,19 +7,21 @@ Description: Lab 3 Question 3 (lab5c.py).
 import random
 
 animals = ['snake', 'hamster', 'scorpion', 'beaver', 'mosquito', 'camel', 'vulture', 'horse', 'python', 'capybara' ]
-secret = random.choice(animals)
+secret = random.choice(animals)  # Choose a random animal from the list as the secret word
 
 while True:
+    
     print("I'm thinking of an animal. Can you guess what it is?")
-    guess = input("Enter a letter or a guess. Press enter to quit:")
-    if guess == "":
+    guess = input("Enter a letter or a guess. Press enter to quit:") #user input (guess)
+
+    if guess == "": #exit the loop
         break
-    elif len(guess) >1 and guess == secret:
+    elif len(guess) >1 and guess == secret: #guess correct word
         print("You win!")
         break
-    elif len(guess) == 1 and guess in secret:
+    elif len(guess) == 1 and guess in secret: #guess a letter contain in the secret
         print("Yes, my word contains that letter.")
-    elif len(guess) == 1:
+    elif len(guess) == 1:  #guess a letter not contain in the secret
         print("Sorry, my word doesn't contain that letter.")
-    else:
+    else:  #guess a wrong word
         print("Sorry, that's not it.")

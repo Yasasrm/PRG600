5c44a2738db320fa95081677bdd2fadf1194d99c
Yasas Maddumage <yasasrm@gmail.com>
2024-06-08 22:28:42 -0400

    Comment added

--- Assingments/Assingment02/DiscountCalculator.py
+++ Assingments/Assingment02/DiscountCalculator.py
@@ -1,11 +1,11 @@
 ''' 
 Team Members: 
-· Hasini Jayasekara - 165513235
+    · Hasini Jayasekara - 165513235
     · Yasas Maddumage - 170308233
     · Sayona Perera - 169835238
     · Iresha Lakmali - 162279236
     · Nimshi Fernando - 170739239
-    Description: Assigment 2 Discount Calculator.  
+Description: Assigment 2 Discount Calculator.  
 ''' 
 
 discountedList =  [

da6fb93750410db89f282265824b05e58badc1a2
Yasas Maddumage <yasasrm@gmail.com>
2024-07-04 23:43:43 -0400

    File reading

--- Labs/Lab_04/lab6a.py
+++ Labs/Lab_04/lab6a.py
@@ -6,7 +6,7 @@ Description: Lab 4 Question 1 (lab6a.py).
 
 data_to_write = ['First Line!', 'Second Line!!', 'Third Line!!!', '...and so on!']
 
-f = open('testing.txt', 'w')  #Create a file
+f = open('testing.txt', 'w')  #Create a file object
 for line in data_to_write:    #Get next line
     f.write(line + '\n')      #Write next line
 f.close()                     #close file object
---
+++ Labs/Lab_04/lab6b.py
@@ -0,0 +1,18 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 4 Question 2 (lab6b.py).  
+''' 
+
+import sys
+
+def reverseFilePrinter(fileName):
+    f = open(fileName, 'r')  #Create a file object
+    content = f.readlines()  #Load file content
+    content.reverse()        #Reverse file content
+    for line in content:     #Print content
+        print(line.strip())
+
+if __name__ == "__main__":
+    fileName = 'readme.txt' if len(sys.argv) == 1 else sys.argv[1] # Use 'readme.txt' if no command line argument is provided
+    reverseFilePrinter(fileName)

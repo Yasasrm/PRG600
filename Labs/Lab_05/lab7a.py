6c5ac6e32a9caf3bb0bea3487ea4baa2d82f3bb0
Yasas Maddumage <yasasrm@gmail.com>
2024-07-05 16:06:52 -0400

    Print dictionary

--- Labs/Lab_05/lab7a.py
+++ Labs/Lab_05/lab7a.py
@@ -4,14 +4,14 @@ Student ID: 170308233.
 Description: Lab 5 Question 1 (lab7a.py).  
 ''' 
 
-student1 = ['eric', 'smith', '217 Au Large Blvd', 'Toronto', 'Ontario', 'M4A 1P3']
+student1 = {'first_name': 'eric', 'last_name':'smith', 'addr1': '217 Au Large Blvd', 'city': 'Toronto', 'prov': 'Ontario', 'pcode': 'M4A 1P3'}
 
 def shipping_label(a_lst): 
     "takes a list, generates a string" 
-    a_str = f"{a_lst[0]} {a_lst[1]}\n" 
-    a_str += f"{a_lst[2]}\n" 
-    a_str += f"{a_lst[3]}, {a_lst[4]}\n"
-    a_str += f"{a_lst[5]}" 
+    a_str = f"{a_lst['first_name']} {a_lst['last_name']}\n".upper() 
+    a_str += f"{a_lst['addr1']}\n" 
+    a_str += f"{a_lst['city']}, {a_lst['prov']}\n"
+    a_str += f"{a_lst['pcode']}" 
     return a_str
 
 if __name__ == "__main__":

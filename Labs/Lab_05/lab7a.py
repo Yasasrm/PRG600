d181fd4a17143860cce5f1a5bd65b4f821975da2
Yasas Maddumage <yasasrm@gmail.com>
2024-07-05 16:10:11 -0400

    7a bug fix (name first letter uppercase)

--- Labs/Lab_05/lab7a.py
+++ Labs/Lab_05/lab7a.py
@@ -8,7 +8,7 @@ student1 = {'first_name': 'eric', 'last_name':'smith', 'addr1': '217 Au Large Bl
 
 def shipping_label(a_lst): 
     "takes a list, generates a string" 
-    a_str = f"{a_lst['first_name']} {a_lst['last_name']}\n".upper() 
+    a_str = f"{a_lst['first_name'].capitalize() } {a_lst['last_name'].capitalize() }\n"
     a_str += f"{a_lst['addr1']}\n" 
     a_str += f"{a_lst['city']}, {a_lst['prov']}\n"
     a_str += f"{a_lst['pcode']}" 

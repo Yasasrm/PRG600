45b812463ce16399f7e00cd28104573ea1541401
Yasas Maddumage <yasasrm@gmail.com>
2024-07-04 23:19:00 -0400

    write to a file in a loop

--- Labs/Lab_04/lab6a.py
+++ Labs/Lab_04/lab6a.py
@@ -6,7 +6,7 @@ Description: Lab 4 Question 1 (lab6a.py).
 
 data_to_write = ['First Line!', 'Second Line!!', 'Third Line!!!', '...and so on!']
 
-f = open('testing.txt', 'w') 
-f.write('This is a new test sentence!') 
-f.write('This is the second line(?)') 
-f.close()
+f = open('testing.txt', 'w')  #Create a file
+for line in data_to_write:    #Get next line
+    f.write(line + '\n')      #Write next line
+f.close()                     #close file object

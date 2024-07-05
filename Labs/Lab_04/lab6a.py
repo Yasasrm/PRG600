b4f591161823c604e2688071641838eaf1734d86
Yasas Maddumage <yasasrm@gmail.com>
2024-07-04 23:08:12 -0400

    write to a file

---
+++ Labs/Lab_04/lab6a.py
@@ -0,0 +1,11 @@
+''' 
+Name: Yasas Maddumage. 
+Student ID: 170308233. 
+Description: Lab 4 Question 1 (lab6a.py).  
+''' 
+
+data_to_write = ['First Line!', 'Second Line!!', 'Third Line!!!', '...and so on!']
+
+f = open('testing.txt', 'w') 
+f.write('This is a test sentence.') 
+f.close()
